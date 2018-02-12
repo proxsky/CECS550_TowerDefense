32b91cc5c603af4df74404a0349755ab65dfc08e
jtong <jstong01@louisville.edu>
2018-02-12 15:34:47 -0500

    Initial UI work
    Initial work. This is the layout, scripts to be done soon.

---
+++ Assets/Scripts/PlaceTurret.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTurret : MonoBehaviour {
+    public GameObject turretPrefab;
+    private GameObject turret;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private bool canPlaceTurret() {
+        return turret == null;
+    }
+}
