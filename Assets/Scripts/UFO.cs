7900d6daa43cb30c683f8d93d421e6d9249eae3a
RickyH <xproxskyx@gmail.com>
2018-03-08 22:44:00 -0500

    UFO Spawner

---
+++ Assets/Scripts/UFO.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO : MonoBehaviour {
+        
+	void Update () {
+        transform.Rotate(Vector3.up, 50 * Time.deltaTime);
+    }
+}
